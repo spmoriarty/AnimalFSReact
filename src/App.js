56a62d2c75aeb9f9452e4f4a1eea652a997b67d2
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-07 12:33:09 -0700

    working background rendering out of main.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
+
+
+
+
 function App() {
   return (
     <div className="App">
