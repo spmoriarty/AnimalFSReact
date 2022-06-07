5ff549acd53be5137f01bf5659feabcfe9240b77
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-07 12:42:22 -0700

    creating animal card

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function Animal() {
-  return <></>;
+export default function Animal(name, type, says) {
+  return <div className='animal-card'>
+    <h1>{name}</h1>
+    <p>{type}</p>
+    <p>{says}</p>
+    {/* <img src = {`/images/${name}.png`} /> */}
+  </div>;
+
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Main.css';
 import backgroundImage from '../../background.png';
+import animals from '../Animal.js';
+
 
 export default function Main() {
   return <main className= 'background' style={{ backgroundImage: `url(${ backgroundImage })` }}></main>;
 }
+
